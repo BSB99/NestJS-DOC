96e6c53349457a6c0f6593ab4d4240b8f05fb0d2
BaekSeungBum <wer06099@naver.com>
2022-11-15 14:59:36 +0900

    Refactor(Seungbum) DTO 양식 변경 #4

--- src/models/users/dto/sign-up.dto.ts
+++ src/models/users/dto/sign-up.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsString, Matches } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, Length, Matches } from "class-validator";
 
 export class SignUpDto {
     @ApiProperty({
@@ -7,23 +7,19 @@ export class SignUpDto {
         description: 'email를 입력해 주세요'
     })
     @IsEmail({message: 'email 형식에 맞게 입력해 주세요.'})
-    @IsNotEmpty({message: 'email을 입력해 주세요.'})
     email: string;
 
     @ApiProperty({
         example: 'test',
         description: 'id를 입력해 주세요'
     })
-    @IsString({message: 'id: 문자만 입력해 주세요.',})
-    @IsNotEmpty({message: 'id를 입력해 주세요.'})
+    @Length(3, 16, {message: 'id: 3자 ~ 16자'})
     id: string;
 
     @ApiProperty({
         example: '1234',
         description: 'password를 입력해 주세요'
     })
-    @IsString({message: 'password: 문자만 입력해 주세요.'})
-    @IsNotEmpty({message: 'password를 입력해 주세요.'})
     @Matches( /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{10,20}$/,
     {
     message: '비밀번호 양식에 맞춰 작성해 주세요.(최소 하나의 숫자/대문자/소문자/특수문자) , 10자 ~ 20자)'
@@ -34,7 +30,6 @@ export class SignUpDto {
         example: 'name',
         description: '이름을 입력해 주세요'
     })
-    @IsString({message: 'name: 문자만 입력해 주세요.'})
-    @IsNotEmpty({message: 'name를 입력해 주세요.'})
+    @Length(1, 8, {message: 'name: 1자 ~ 8자'})
     name: string;
 }
