07e05143fc047334b8663d60bdcd453f2d8de45d
BaekSeungBum <wer06099@naver.com>
2022-11-15 14:21:35 +0900

    Add(Seungbum): password 양식 설정 #3

--- src/models/users/dto/sign-in.dto.ts
+++ src/models/users/dto/sign-in.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNotEmpty, IsNumber, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString, Matches } from "class-validator";
 
 export class SignInDto {
     @ApiProperty({
@@ -16,5 +16,9 @@ export class SignInDto {
     })
     @IsString({message: 'password: 문자만 입력해 주세요.'})
     @IsNotEmpty({message: 'password를 입력해 주세요.'})
+    @Matches( /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+    {
+    message: '비밀번호 양식에 맞춰 작성해 주세요.(숫자/대문자/소문자/특수문자) , 최소 8자)'
+    })
     password: string;
 }
--- src/models/users/dto/sign-up.dto.ts
+++ src/models/users/dto/sign-up.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsEmail, IsNotEmpty, IsString } from "class-validator";
+import { IsEmail, IsNotEmpty, IsString, Matches } from "class-validator";
 
 export class SignUpDto {
     @ApiProperty({
@@ -24,6 +24,10 @@ export class SignUpDto {
     })
     @IsString({message: 'password: 문자만 입력해 주세요.'})
     @IsNotEmpty({message: 'password를 입력해 주세요.'})
+    @Matches( /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/,
+    {
+    message: '비밀번호 양식에 맞춰 작성해 주세요.(숫자/대문자/소문자/특수문자) , 최소 8자)'
+    })
     password: string;
 
     @ApiProperty({
