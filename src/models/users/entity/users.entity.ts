3e03b5c5e2bb39f815ab26a99543889f8c8180b8
BaekSeungBum <wer06099@naver.com>
2022-11-16 15:22:03 +0900

    Add(Seungbum): user entity 추가 #3

--- src/models/users/entity/users.entity.ts
+++ src/models/users/entity/users.entity.ts
@@ -1,4 +1,4 @@
-import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { BaseEntity, Column, CreateDateColumn, DeleteDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity('users')
 export class User extends BaseEntity{
@@ -25,12 +25,77 @@ export class User extends BaseEntity{
     name: string;
 
     @Column({
+        type: 'varchar',
+        length: 255
+    })
+    image: string;
+
+    @Column({
+        type: 'date'
+    })
+    date_birth: Date;
+
+    @Column({
+        type: 'varchar',
+        length: 20
+    })
+    phone_number: string;
+
+    @Column({
+        type: 'mediumtext',
+    })
+    introduce: string;
+
+    @Column({
+        type: 'int'
+    })
+    language: number;
+
+    @Column({
+        type: 'tinyint'
+    })
+    service_agreement: boolean;
+
+    @Column({
+        type: 'tinyint'
+    })
+    privacy_terms: boolean;
+
+    @Column({
+        type: 'tinyint'
+    })
+    cookie_policy: boolean;
+
+    @Column({
+        type: 'tinyint'
+    })
+    marketing_terms: boolean;
+    
+    @Column({
+        type: 'tinyint'
+    })
+    active: boolean;
+
+    @Column({
+        type: 'varchar',
+        length: 50
+    })
+    otp_secret: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date|null;
+
+    @DeleteDateColumn({
+        nullable: true
+    })                                                                                                                                                                                                                                                  
+    deletedAt: Date|null
+
+    // 지울예정
+    @Column({
         nullable: true
     })
     refreshToken: string;
-
-    @Column({
-        default: false
-    })
-    active: boolean;
 }
